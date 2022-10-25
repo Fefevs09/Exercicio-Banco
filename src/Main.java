21a0dd4e955e4e3a23c5e00b0878fa7c16de1597
Felipe Vieira <felipefernandes.contato09@gmail.com>
2022-10-25 17:34:26 -0300

    adicionado opcoes de tipo de conta

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     String nomeCliente;
     String numConta;
+    int resposta;
     float vlrSaldo;
 
     // na linha abaixo estamos instânciando o objeto minhaConta do tipo Conta
-    Conta minhaConta = new Conta();
     System.out.println(" ******* Programa do Banco do Brasil ******* ");
     System.out.print("Informe o seu nome: ");
     nomeCliente = sc.nextLine();
@@ -17,12 +17,31 @@ public class Main {
     System.out.print("Informe o seu saldo: ");
     vlrSaldo = sc.nextFloat();
 
-    // na linha abaixo estamos abrindo a conta do cliente
-    if (minhaConta.AbrirConta(nomeCliente, numConta, vlrSaldo)) {
-      System.out.println("Parabéns! Conta aberta com sucesso!");
-    } else {
-      System.out.println("Conta não foi aberta!");
+    System.out.println("**** Escolha o Tipo da Conta ****");
+    System.out.println(" 1) Corrente");
+    System.out.println(" 2) Poupança");
+    System.out.println(" 3) Salario");
+    System.out.println(" 4) Sair");
+    System.out.println("Digite a opção desejada: ");
+    resposta = sc.nextInt();
+
+    Conta minhaConta = null;
+    switch (resposta) {
+      case 1:
+        minhaConta = new Conta();
+        break;
+      case 2: 
+        minhaConta = new Poupanca();
+      case 3: 
+        minhaConta = new Salario();
+        break;
+      default:
+        System.out.println("Opção não é válida!");
+        break;
     }
+    // na linha abaixo estamos abrindo a conta do cliente
+      minhaConta.AbrirConta(nomeCliente, numConta, vlrSaldo);   
+
     int opcao;
     do {
       // Na lihha abaixo estamos chamando o metodo ExibirMenu do objeto minhaConta da
@@ -36,25 +55,22 @@ public class Main {
         case 2:
           System.out.print("Insira o valor do deposito: ");
           float vlrDep = sc.nextFloat();
-          if (vlrDep <= 0) {
-            System.out.println("\nValor inválido");
-          } else if (minhaConta.Depositar(vlrDep)) {
-            System.out.println("O depósito ocorreu com sucesso");
-          } else {
+          if (minhaConta.Depositar(vlrDep)) 
+            System.out.println("O depósito " + vlrDep + " ocorreu com sucesso");
+          else 
             System.out.println("Erro ao fazer o deposito");
-          }
+          
           break;
 
         case 3:
           System.out.print("Insira o valor do saque: ");
           float vlrSaq = sc.nextFloat();
-          if (vlrSaq <= 0) {
-            System.out.println("\nValor inválido");
-          } else if (minhaConta.Depositar(vlrSaq)) {
-            System.out.println("O saque ocorreu com sucesso");
-          } else {
+
+          if (minhaConta.Depositar(vlrSaq)) 
+            System.out.println("O saque de " + vlrSaq + " ocorreu com sucesso");
+          else 
             System.out.println("Erro ao fazer o saque");
-          }
+          
           break;
 
         case 4:
@@ -67,6 +83,7 @@ public class Main {
       }
 
     } while (opcao != 4);
+
     sc.close();
 
   }
